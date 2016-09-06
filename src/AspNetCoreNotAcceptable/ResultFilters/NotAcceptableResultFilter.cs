5af26ca5f79a861d2c551b650111c8261219c5d8
Darryn Patterson <darryn@whereismytransport.com>
2016-09-06 09:20:55 +0200

    Added the ResultFilter and updated solution version.

---
+++ src/AspNetCoreNotAcceptable/ResultFilters/NotAcceptableResultFilter.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCoreNotAcceptable.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetCoreNotAcceptable.ResultFilters
+{
+    public class NotAcceptableResultFilter : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var result = context.Result as StatusCodeResult;
+
+            if (result != null && result.StatusCode == 406)
+            {
+                context.Result = new HttpNotAcceptableResult();
+            }
+        }
+    }
+}
